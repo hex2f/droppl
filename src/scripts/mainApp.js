f76f14c7552831458062c524ecdff4b4706e5208
RekkyRek <haslun16@edu.vilhelmina.se>
2017-02-15 16:26:53 +0100

    Font Awesome and small ui tweaks. Mainly push to dev on desktop.

--- src/scripts/mainApp.js
+++ src/scripts/mainApp.js
@@ -1,5 +1,6 @@
 var {ipcRenderer, remote} = require('electron');
 var main = remote.require('./src/index.js');
+var anime = require('animejs');
 
 if(localStorage.getItem('domainHost') == null) {
   notJoinedCard.style.display = "block"
@@ -12,4 +13,23 @@ function domainConnect() {
       window.location.reload()
     }
   },500);
+}
+
+function openMenu() {
+  var menu_bar = document.getElementById('menu_bar');
+  if(menu_bar.style.height < 75 || menu_bar.style.height == "0px") {
+    anime({
+      targets: '#menu_bar',
+      height: 175,
+      duration: 500,
+      easing: 'easeInOutCubic'
+    });
+  } else {
+    anime({
+      targets: '#menu_bar',
+      height: 0,
+      duration: 500,
+      easing: 'easeInOutCubic'
+    });
+  }
 }
