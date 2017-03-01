1407c830fc8bb6aee77bc1191d2dcf3c6a454b66
Unknown <haslun16@edu.vilhelmina.se>
2017-03-01 21:00:22 +0100

    Tried a bit more. Did not get it to work :(  (error: browser-only)

--- src/scripts/viewer.js
+++ src/scripts/viewer.js
@@ -1,18 +1,19 @@
 /*Not needed yet.
-var Vue = require('../vue.dev.js');
-var $ = require('jQuery');*/
+var Vue = require('../vue.dev.js');*/
+var $ = require('jQuery');
 
 var {remote} = require('electron');
 var main = remote.require('./src/index.js');
 
 var ipc = require('electron').ipcRenderer;
+var file;
+
+console.log(file);
 
 ipc.on('openFile' , function(event , magnet){
 	console.log(magnet);
-
-	main.webtorrent.get(magnet, (torrent) => {
-		var file = torrent.files[0];
-		console.log(file);
-		file.appendTo('body');
-	});
+	var torrent = main.webtorrent.get(magnet);
+	console.log(torrent);
+	file = torrent.files[0];
+	file.renderTo('#player')
 });
